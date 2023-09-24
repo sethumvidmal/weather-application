9005f5d272902e17e451f16948b55a0ce3758a36
Sethum Vidmal <sethumthabrew@gmail.com>
2023-09-24 21:01:07 +0530

    Dark mode button

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,11 @@
 'use strict';
 function searchBtnOnClick() {
-    let location = document.getElementById("searchField");
-    console.log(location.value);
+    // let location = document.getElementById("searchField");
+    // console.log(location.value);
+    // const cityName = document.getElementById("cityName");
+    // cityName.innerText = location.value;
+    const searchField = $("#searchField")
+    console.log(searchField.val());
+    let typedText = searchField.val();
+    
 }
