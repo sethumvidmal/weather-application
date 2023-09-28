97d3a3625d2813e1d96d78804eb2c9f1aafa61a9
Sethum Vidmal <sethumthabrew@gmail.com>
2023-09-28 19:58:49 +0530

    Some changes

--- Js/script.js
+++ Js/script.js
@@ -1,11 +1,30 @@
-'use strict';
+`use strict`;
+const date = new Date();
 function searchBtnOnClick() {
-    // let location = document.getElementById("searchField");
-    // console.log(location.value);
-    // const cityName = document.getElementById("cityName");
-    // cityName.innerText = location.value;
-    const searchField = $("#searchField")
-    console.log(searchField.val());
-    let typedText = searchField.val();
-    
+    let location = document.getElementById(`searchField`);
+    currentWeather(location.value);
+}
+function loadDefaultLocation() {
+    currentWeather(`colombo`);
+};
+//input field value gose here
+function currentWeather(location) {
+    fetch(`https://api.weatherapi.com/v1/current.json?q=${location}&key=be613ef740984fda9ae155111231909`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            document.querySelector(`.currentTemp`).innerHTML = data.current.temp_c + `<sup>o</sup>C`;
+            document.querySelector(`.currentState`).innerHTML = data.current.condition.text;
+            document.querySelector(`.lblWind`).innerHTML = data.current.wind_mph + `mph`;
+            document.querySelector(`.lblHumidity`).innerHTML = data.current.humidity + `%`;
+            document.querySelector(`.lblPressure`).innerHTML = data.current.pressure_mb + `mb`;
+            document.querySelector(`.lblUv`).innerHTML = data.current.uv;
+            document.querySelector(`.lblCloudCover`).innerHTML = data.current.cloud + `%`;
+            document.querySelector(`.lblPrecipitation`).innerHTML = data.current.precip_mm;
+            document.querySelector(`.lblLocation`).innerHTML = `${data.location.name}, ${data.location.country}`;
+            document.querySelector(`.icon`).innerHTML = `<img src="${data.current.condition.icon}" class="icon">`;
+        })
+}
+function switchTheme() {
+
 }
